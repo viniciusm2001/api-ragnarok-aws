f6e37b7ff494a1462cdd2e8197cd03c218b8f417
Vinicius M <vini@meuemail.com>
2019-11-04 21:24:46 -0300

    a

--- reset_fotos.bat
+++
@@ -1,3 +0,0 @@
-del /q fotos\* >nul
-for /d %%x in (fotos\*) do @rd /s /q "%%x" >nul
-Xcopy /E /I bkp\fotos fotos >nul
--- utils/Round.js
+++
@@ -1,6 +0,0 @@
-const round = (valor, precisao) => {
-   const multiplicador = Math.pow(10, precisao || 0);
-   return Math.round(valor * multiplicador) / multiplicador;
-}
-
-module.exports = round;
