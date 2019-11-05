f6cec095fff68718eda9d1c8a2d059cdfc1660fc
Vinicius M <vini@meuemail.com>
2019-11-04 21:33:21 -0300

    opa kk vo comita

---
+++ config/conexao.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+
+const database = "db_ragnarok";
+const user = "root";
+const password = "bcd127";
+const host = "localhost";
+const port = 3306;
+
+const con = new Sequelize(database, user, password, {
+   host,
+   port,
+   dialect: 'mysql',
+   logging: false
+});
+
+module.exports = con;
